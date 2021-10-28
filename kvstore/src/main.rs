d24c2aa7213818562df00bc82652e8dbc74e97c4
Nicolas Durpoix <nicolas.durpoix82@gmail.com>
2021-10-28 18:58:56 +0200

    removmed db file

--- kvstore/src/main.rs
+++ kvstore/src/main.rs
@@ -1,32 +1,38 @@
-use std::{collections::HashMap, error::Error};
+use std::{collections::HashMap};
 
 fn main() {
     let mut args = std::env::args().skip(1); // gets the arguments from the command line, skips the first element because it is the executable path
-    let key = args.next().unwrap();
-    let value = args.next().unwrap();
+    
+    /*
+      unwrap() throws an error if the method next() fails and the value is null
+    */
+    let key = args.next().unwrap(); // gets the next argument
+    let value = args.next().unwrap(); // gets the next argument
 
     let contents = format!("{}\t{}\n", key, value);
-    std::fs::write("kv.db", contents);
-
+    //std::fs::write("kv.db", contents);
     println!("Key: {:?}  | Value: {:?}", key, value);
 
     // Run with arguments  : cargo r -- arg arg
 
+    let database = Database::new();
+
 }
 
 struct Database {
-    inner: HashMap<String, String>,
+    map: HashMap<String, String>,
 }
 
 impl Database {
     fn new() -> Result<Database, std::io::Error> {
-        let contents = match std::fs::read_to_string("kv.db") {
-            Ok(c) => c,
-            Err(e) => return Err(e)
-        };
+        // let contents = match std::fs::read_to_string("kv.db") {
+        //     Ok(c) => c,
+        //     Err(e) => return Err(e)
+        // };
+        /* Same thing as above */ let contents = std::fs::read_to_string("kv.db")?;
 
         Ok(Database{
-            inner: HashMap::new()
+            map: HashMap::new()
         })
     }
 }
