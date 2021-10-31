63e71536dedaa79ed50448c9e414bbb731f12189
Nicolas Durpoix <nicolas.durpoix82@gmail.com>
2021-10-31 23:17:50 +0100

    init

--- rust-api/src/main.rs
+++ rust-api/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
 }
+
+fn main() {
+    let db_connection = rusqlite::Connection::open("data.sqlite");
+    rocket::ignite().mount("/", routes![index]).launch();
+}
+
